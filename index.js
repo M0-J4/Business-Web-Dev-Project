d4762aa0a8c29aed0a8d00bde54f04913c0d5066
Michael Jones <mrj705@outlook.com>
2021-03-10 18:17:59 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,35 @@
+// import environment variables store in the .env file
+ require('dotenv').config();
+
+const fetch = require('node-fetch');
+
+const {grab} = require('./apiAuth');
+
+const app = express();
+
+
+// handle the middleware stuffs
+app.use('/', express.static('public'));
+
+
+// go to http://localhost:portnumber/wolverine
+app.get('/wolverine', (req, res, next) =>
+{
+    grab('characters', {name: 'Wolverine'})
+    .then((data) =>
+    {
+        console.log('data: ', data);
+        res.json(data);
+    })
+    .catch((err) =>
+    {
+        console.log('Err: ', err);
+    })
+
+
+});
+
+app.listen(process.env.PORT, () =>
+{
+    console.log(`Server is listening on port ${process.env.PORT}...`);
+})
